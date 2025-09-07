ea4fb2bfd28d9614d6a344b381babbc142fc48b3
Ahsan Alam <ahsansfiverr@gmail.com>
2025-09-07 15:58:03 +0600

    creating the initial files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: { appDir: true },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
